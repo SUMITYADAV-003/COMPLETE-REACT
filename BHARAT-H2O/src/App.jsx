bcf901ce4d8ae9c40e80b5431b909d849196aa00
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-01-27 21:28:00 +0530

    Search bar are done

--- BHARAT-H2O/src/App.jsx
+++ BHARAT-H2O/src/App.jsx
@@ -5,13 +5,14 @@ import "./app.scss";
 import HomePage from './HeroPage/HomePage';
 import About from './HeroPage/About';
 import Contact from './HeroPage/Contact';
-
+import SearchBar from './Component/SearchBar';
 
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <SearchBar />
      <Routes>
       <Route path='/' element={<HomePage/>} />
       <Route path='/about' element={<About/>} />
